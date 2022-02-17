d8ca9c9ecb72227d7d9bf1515e9d935d425ae5e6
Mxyb <97235899+Mxyb@users.noreply.github.com>
2022-02-17 10:58:24 +0000

    Initial Commit

---
+++ CollectionPageV1/CollectionPageV1App.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionPageV1App.swift
+//  CollectionPageV1
+//
+//  Created by Max Baker on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CollectionPageV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
